534345d0b1081061669959812232f2d4955bca17
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2021-01-29 17:40:05 +0000

    Basic music player

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# discord-bot-py
+# Discord Bot
+
+Akame Bot
+
+# Config
+
+Create your config.json file
+
+```
+{
+    "token": "AAAA" // Your discord bot token
+}
+```
+
+Make sure both packages are installed
+> Python
+> FFmpeg
+
+Python requires the following modules
+> python -m pip install discord
+> python -m pip install ytdl
+> python -m pip install PyNaCl
+> python -m pip install beautifulsoup4
---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                   description='Akame Bot')
+
+# Costum Modules
+from music import *
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0} ({0.id})'.format(bot.user))
+    print('------')
+
+@bot.command()
+async def ping(ctx):
+    """Pong"""
+    embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
+    embedVar.add_field(name="Field1", value="hi", inline=False)
+    embedVar.add_field(name="Field2", value="hi2", inline=False)
+    await ctx.send(embed=embedVar)
+
+if __name__ == "__main__":
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        token = data['token']
+    
+    bot.add_cog(Music(bot))
+    bot.run(token)
